7a63802d69f35160e798b50b31b2993945106f01
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-02-20 19:49:18 +0300

    counter app finished

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Connection, PublicKey } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+import idl from './idl.json';
+
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const wallets = [ new PhantomWalletAdapter() ];
+
+const { SystemProgram, Keypair } = web3;
+const baseAccount = Keypair.generate();
+const opts = {
+  preflightComponent: "processed"
+};
+const programID = new PublicKey(idl.metadata.address);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [value, setValue] = useState('');
+  const wallet = useWallet()
+
+  async function getProvider() {
+    const network = "http:127.0.0.1:8899";
+    const connection = new Connection(network, opts.preflightComponent)
+
+    const provider = new Provider(
+      connection, wallet, opts.preflightComponent,
+    );
+
+    return provider;
+  }
+
+  async function createCounter() {
+    const provider = await getProvider();
+
+    const program = new Program(idl, programID, provider);
+
+    try {
+      await program.rpc.create({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      console.log('account: ', account);
+      setValue(account.count.toString());
+    } catch(err) {
+      console.log("Transaction error: ", err);
+    }
+  }
+
+  async function incrementCounter() {
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("account: ", account);
+    setValue(account.count.toString());
+  }
+
+  if (!wallet.connected) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', mainTop: '100px' }}>
+        <WalletMultiButton />
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        <div>
+          {
+            !value && (<button onClick={createCounter}>Create Counter</button>)
+          }
+          {
+            value && (<button onClick={incrementCounter}>Increment Counter</button>)
+          }
+
+          {
+            value && value >= Number(0) ? (
+              <h2>{ value }</h2>
+            ) : (
+              <h3>Please create the counter.</h3>
+            )
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+const AppWithProvider = () => (
+  <ConnectionProvider endpoint="http://127.0.0.1:8899">
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
+);
+
+export default AppWithProvider;
