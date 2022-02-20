e17cb9748e06e095d5b191f87744c1d6e8d25472
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-02-20 23:12:38 +0300

    messaging smart contract is completed

--- programs/fullstack-solana/src/lib.rs
+++ programs/fullstack-solana/src/lib.rs
@@ -6,6 +6,22 @@ declare_id!("14DsznRiz2EztSCCWvzv4KdS7TtyjEMjkbKqxr5ui8vh");
 mod fullstack_solana {
     use super::*;
 
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
+        Ok(())
+    }
+
     pub fn create(ctx: Context<Create>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         base_account.count = 0;
@@ -17,6 +33,22 @@ mod fullstack_solana {
         base_account.count += 1;
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 64)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
 }
 
 // Transaction instruction
@@ -41,4 +73,6 @@ pub struct Increment<'info> {
 #[account]
 pub struct BaseAccount {
     pub count: u64,
+    pub data: String,
+    pub data_list: Vec<String>,
 }
